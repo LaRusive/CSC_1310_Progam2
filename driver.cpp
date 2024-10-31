86882eb3fd56535e7729bbb103af2585eb9f634e
jseger05 <jseger42@tntech.edu>
2024-10-31 17:39:59 -0500

    Added to-do list to driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -4,7 +4,26 @@
 
 int main()
 {
-    
+    /*
+    To do:
+        In LinkedList, swap out bubble sort for a different sort and add Friend  << overload (use displayList())
+
+        Add at least one more sort to go the opposite way. Would be cool to also add sorts for different attributes -- right now it defaults to
+            sort by author, maybe add a sort by year?
+
+        Implement either class list node or smart pointer. List node should be easy to just remove the list node struct from LinkedList and 
+            make it its own thing so I'd reccomend that
+        
+        Figure out MakeFile -- shouldn't be too hard
+
+        Write test case
+
+        Fill out bookData.txt
+
+        Put together Driver
+            Program flow -- menus and such
+            File import/export
+    */
 
     return 0;
 }
